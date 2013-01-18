92f5ccdba2ff818ee3ead6d3c5385359a7cb713b
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-01-18 20:08:29 +0200

    test to app

--- app.js
+++ app.js
@@ -1,40 +1,59 @@
 var fs = require('fs');
-
 var five = require("johnny-five")
 var tako = require('tako')
 
 var app = tako()
-
 app.route('/').file(__dirname + '/client/index.html');
 app.route('/*').files(__dirname + '/client');
-
 app.httpServer.listen(8000)
 
-app.sockets.on('connection', function (socket) {
-  var value = 0
-  
-  app.sockets.emit('values', { scrollFirst: value});
 
-  var board = new five.Board();
+var meterRed = 0
+var meterWhite = 0
+var delay = 10
 
-  board.on("ready", function() {
+var board = new five.Board();
+
+board.on("ready", function() {
 
     potentiometer = new five.Sensor({
-      pin: "A0",
-      freq: 100
+      pin: "A0"
+      , freq: 5
     });
+    
+    var wait = 0
 
+    app.sockets.on('connection', function (socket) {
+  
+    app.sockets.emit('meterWhite', { value: meterWhite});        
+    app.sockets.emit('meterRed', { value: meterRed});        
+    
     potentiometer.on("read", function( err, v ) {
       var new_value = convertRange(v, 9, 902, 0, 150)
-      if (new_value < value || new_value > value) {
-        value = new_value
-        app.sockets.emit('values', { scrollFirst: value});
+      if (new_value < meterWhite || new_value > meterWhite) {
+        meterWhite = new_value
+        console.log(meterWhite)
+        app.sockets.emit('meterWhite', { value: meterWhite});        
+      } else {
+        wait++
       }
-    
+      if (wait > delay) {
+        if (meterWhite > meterRed) {
+          meterRed++
+          console.log('Climb: ' + meterRed)
+          app.sockets.emit('meterRed', { value: meterRed});        
+        }
+        if (meterWhite < meterRed) {
+          meterRed--
+          console.log('Climb: ' + meterRed)
+          app.sockets.emit('meterRed', { value: meterRed});        
+        }
+        wait = 0
+      }
     });
 
-  })
-  
+})
+
 })
 
 function convertRange(val, old_min, old_max, new_min, new_max) {
