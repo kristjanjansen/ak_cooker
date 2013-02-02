71759df1c881a35da0d25a6235e80f0d1339ddf0
kristjanjansen <kristjan.jansen@gmail.com>
2013-02-02 11:15:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ var tako = require('tako')
 
 var port  = 8000
 
-var host = os.hostname().split('.')
-if (host[host.length - 1] !== 'local') host.push('local')
-
 var app = tako()
-app.route('/config.json').json({host: host.join('.') + ':' + port});
+app.route('/config.json').json({'http://localhost:' + port});
 app.route('/cooker1').file(__dirname + '/client/index.html');
 app.route('/cooker2').file(__dirname + '/client/index2.html');
 app.route('/*').files(__dirname + '/client');
